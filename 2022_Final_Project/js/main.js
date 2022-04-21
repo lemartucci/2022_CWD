7d59bdc84bae1e480d13d83dff30002df11e23da
lemartucci <98621443+lemartucci@users.noreply.github.com>
2022-04-21 18:20:50 -0500

    Basemaps loaded and projected!
    County layer and state layer for midwest states are loaded. I realize that I forgot background states...I will fix

--- 2022_Final_Project/js/main.js
+++ 2022_Final_Project/js/main.js
@@ -2,8 +2,8 @@ window.onload = setMap();
 
 function setMap() {
     //map frame dimensions
-    var width = 960,
-        height = 500;
+    var width = 750,
+        height = 1000;
 
     //create new svg container for the map
     var map = d3
@@ -15,10 +15,10 @@ function setMap() {
 
     //create Albers equal area conic projection centered on Midwest
     var projection = d3.geoAlbers()
-        .center([0, 43.7844])//centered on Midwest
-        .rotate([88.7879, 0, 0])
-        .parallels([89, 38])//Standard parallels
-        .scale(4500)
+        .center([0, 41.60])//centered on Midwest states
+        .rotate([89.35, 0, 0])
+        .parallels([45, 38])//Standard parallels (latitudes)
+        .scale(3800)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
@@ -32,37 +32,36 @@ function setMap() {
     Promise.all(promises).then(callback);//Fetching multiple datasets at once with Promise.All
 
     //Callback function to retrieve the data
-    function callback(data) {
+function callback(data) {
         var midwest = data[0]
             midCounties = data[1]
-            console.log(midwest);
-            console.log(midCounties);
+            //console.log(midwest);
+            //console.log(midCounties);
 
-        //translate TopoJSON to geoJson
+        //translate TopoJSONs to geoJsons
         var midwestStates = topojson.feature(midwest, midwest.objects.Midwest_States_Project);
-       
         console.log(midwestStates);
        
-        var midwestCounties = topojson.feature(counties, counties.objects.USA_Counties_Midwest_Project).features;
+        var midwestCounties = topojson.feature(midCounties, midCounties.objects.USA_Counties_Midwest_Project).features;
         console.log(midwestCounties);
 
     
         //add midwest states to the map
         var midwestBackground = map
             .append("path")
-            .datum(midwest)
-            .attr("class", "area")
+            .datum(midwestStates)
+            .attr("class", "midwestBackground")
             .attr("d", path);
         
         
         //add midwest counties to the map
         var counties = map
             .selectAll(".counties")
-            .data(midCounties)
+            .data(midwestCounties)
             .enter()
             .append("path")
             .attr("class", function (d) {
-                return "counties" + d.properties.counties;
+                return "midwestCounties" + d.properties.STATE_NAME;
             })
             .attr("d", path);//d defines the coordinates of path
         }
