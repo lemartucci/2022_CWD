dda183aa730a4aa8b25cb1380695fabd0f5ab5fc
lhwcodes <98495495+lhwcodes@users.noreply.github.com>
2022-05-09 11:48:32 -0500

    Highlighting fully functional
    Labels need work

--- 2022_Final_Project/js/main.js
+++ 2022_Final_Project/js/main.js
@@ -291,9 +291,10 @@
                       // Loop through each symbol / key
                       dataNest.forEach(function(d,i) {
                           graph.append("path")
-                                .attr("class", function(d){
+                                .datum(d)
+                                .attr("class", function(){
                                     console.log(i)
-                                    return "lines " + d;
+                                    return "lines " + d.key;
                                 })
                               .style("stroke", function() { // Add the colours dynamically
                                   return d.color = color(d.key); })
@@ -527,7 +528,7 @@
                 var selectedpoint = d3.selectAll("." + props.STATE_NAME)
                     .style("stroke", "#536D5E")
                     .style("stroke-width", "3");
-                var selected = d3.selectAll(".lines")
+                var selected = d3.selectAll("." + d.key)
                     .style("stroke", "blue")
                     .style("stroke-width", "2");
                 setLabel(props)
